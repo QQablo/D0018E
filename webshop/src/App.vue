3a6b395772718b0cea4aca7917bbd2dc8df39bee
Yasin <yasin.khalili@outlook.com>
2025-01-30 18:28:01 +0100

    new commit

--- webshop/src/App.vue
+++ webshop/src/App.vue
@@ -19,12 +19,18 @@ export default {
   },
   setup() {
     const products = reactive([
-      { image: 'Adidas_Samba.jpg', name: 'Adidas Samba', description: 'Adidas', price: 69.99 },
+    { image: 'Adidas_Samba.jpg', name: 'Adidas Samba', description: 'Adidas', price: 69.99 },
       { image: 'BOSS_Sienne.jpg', name: 'BOSS Sienne', description: 'BOSS', price: 59.99 },
       { image: 'DIOR_Boy.jpg', name: 'DIOR Boy', description: 'DIOR', price: 99.99 },
       { image: 'DIOR_Timeless.jpg', name: 'DIOR Timeless', description: 'DIOR', price: 79.99 },
-      { image: 'Doc-Martens_1460.jpg', name: 'Doc-Martens 1460', description: 'Doc Martens 1460', price: 59.99 },
+      { image: 'Doc-Martens_1460.jpg', name: 'Doc-Martens 1460', description: 'Doc Martens 1460', price: 99.99 },
+      { image: 'ECCO_Melbourne.jpg', name: 'ECCO Melbourne', description: 'ECCO', price: 119.99 },
+      { image: 'GANT_Wilmon.jpg', name: 'GANT Wilmon', description: 'GANT', price: 19.99 },
+      { image: 'Jack_Jones_Jfwadam.jpg', name: 'Jack & Jones Jfwadam', description: 'Jack & Jones', price: 59.99 },
       { image: 'Nike_Air_Force_1.jpg', name: 'Nike Air Force 1', description: 'Nike', price: 89.99 },
+      { image: 'Nike_Lunar_Force.jpg', name: 'Nike Lunar Force', description: 'Nike', price: 69.99 },
+      { image: 'Pier_One_Eleganta.jpg', name: 'Pier One Eleganta', description: 'Pier One', price: 99.99 },
+      { image: 'Timberlands_6.jpg', name: 'Timberlands 6', description: 'Timberlands', price: 79.99 }
     ]);
 
     return { products };
