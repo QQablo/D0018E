d0f8716d27b5ed427c729e088ce217bb497582bf
Nahor <nahtse-2@student.ltu.se>
2025-02-04 14:06:00 +0100

    hello

--- backend/src/config/db.js
+++ backend/src/config/db.js
@@ -1,13 +1,12 @@
 require('dotenv').config();
 const {Pool} = require('pg');
-// 
 
-const pool = new Pool({ 
-  user: process.env.POSTGRES_USER,
-  host: process.env.POSTGRES_HOST,
-  password: process.env.POSTGRES_PASSWORD,
-  port: process.env.POSTGRES_PORT,
-  database: process.env.POSTGRES_DB,
+const pool = new Pool({
+  user: "student",
+  host: "localhost",
+  password: "student",
+  port: 5430,
+  database: "d0018e",
   max: 10, // Maximum number of connections in the pool
   idleTimeoutMillis: 30000, // Close inactive connections after 30 seconds
 });
