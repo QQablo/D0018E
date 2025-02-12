4db96f9182818c277fb421f01204a1e4e783d37b
Yasin <yasin.khalili@outlook.com>
2025-02-12 10:14:07 +0100

    Connected to database

--- webshop/src/pages/CategoriesList.vue
+++ webshop/src/pages/CategoriesList.vue
@@ -1,20 +1,27 @@
 <template>
-    <div>
-      <h1>Categories</h1>
-      <div v-for="category in categories" :key="category.id">
-        <router-link :to="'/category/' + category.id">
-          <h3>{{ category.name }}</h3>
-        </router-link>
-      </div>
+  <div>
+    <h1>Categories</h1>
+    <div v-for="category in categoriesData.list" :key="category.id">
+      <router-link :to="'/category/' + category.id">
+        <h3>{{ category.name }}</h3>
+      </router-link>
     </div>
-  </template>
-  
-  <script>
-  import { categories } from "@/data.js";
-  
-  export default {
-    setup() {
-      return { categories };
-    }
-  };
-  </script>
+  </div>
+</template>
+
+<script>
+import { reactive } from "vue";
+import axios from "axios";
+
+const categoriesData = reactive({ list: [] });
+
+const ategoriesResponse = async () => {
+  try {
+    const response = await axios.get("http://localhost:3000/api/categories");
+    categoriesData.list = response.data;
+  } catch (error) {
+    console.error("Something went wrong while fetching categories:", error);
+  }
+};
+
+</script>
--- webshop/src/pages/CategoryProducts.vue
+++ webshop/src/pages/CategoryProducts.vue
@@ -1,30 +1,36 @@
 <template>
-    <div>
-      <h1>{{ categoryName }}</h1>
-      <div v-for="product in categoryProducts" :key="product.id" class="product">
-        <img :src="product.image" :alt="product.name" width="100" />
-        <h3>{{ product.name }}</h3>
-        <p>{{ product.description }}</p>
-        <h4>${{ product.price }}</h4>
-        <button>Add to Cart</button>
-      </div>
+  <div>
+    <h1>{{ categoryData.name }}</h1>
+    <div v-for="product in categoryData.products" :key="product.id" class="product">
+      <img :src="product.image" width="150" />
+      <h3>{{ product.name }}</h3>
+      <p>{{ product.description }}</p>
+      <h4>${{ product.price }}</h4>
+      <button>Add to Cart</button>
     </div>
-  </template>
-  
-  <script>
-  import { useRoute } from "vue-router";
-  import { products, categories } from "@/data.js";
-  
-  export default {
-    setup() {
-      const route = useRoute();
-  
-      const categoryId = Number(route.params.id);
-      const categoryProducts = products[categoryId] || [];
-      const categoryName = categories.find((c) => c.id === categoryId)?.name || "Category";
-  
-      return { categoryProducts, categoryName };
-    }
-  };
-  </script>
+  </div>
+</template>
 
+<script>
+//import { useRoute } from "vue-router";
+import { reactive} from "vue";
+import axios from "axios";
+
+const categoryData = reactive({
+  name: "Category",
+  products: []
+});
+
+const products = async () => {
+  try {
+    const categoryResponse = await axios.get(`http://localhost:3000/api/categories/${id}`);
+    category.name = categoryResponse.data.name;
+
+    const productRespons = await axios.get(`http://localhost:3000/api/products?category_id=${id}`);
+    category.products = productRespons.data;
+  } catch (error) {
+    console.error("Something went wrong while fetching products:", error);
+  }
+};
+
+</script>
